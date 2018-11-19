d4c2466d0c418cf6df439bbe45eaa23ea984741d
tilcara <enriquesegovianoandroid@gmail.com>
2018-11-19 11:30:30 -0300

    Login button link to API

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,16 +6,22 @@
       <hr class="my-4">
       <p>This forum is all about community</p>
       <p class="lead">
-        <a class="btn btn-primary btn-lg" href="#" role="button">Sign up</a>
+        <a class="btn btn-primary btn-lg" :href="getLoginURL" role="button">Login with Google</a>
       </p>
     </div>
   </div>
 </template>
-
 <script>
-
-
 export default {
   name: 'home',
+  computed:{
+      getLoginURL() {
+        if(window.location.hostname === 'localhost'){
+          return 'http://localhost:3000/auth/google';
+        }else{
+          return 'https://communityforum.herokuapp.com/auth/google';
+        }
+      }
+  }
 };
 </script>
